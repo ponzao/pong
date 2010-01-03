24067f4d8ed125ebc701399f9d8bee06d670e799
Vesa Marttila <vesa.marttila@helsinki.fi>
2010-01-04 01:24:30 +0200

    hello world is now movable with arrow keys

--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
+function love.load()
+    text = "hello world"
+    x = 400
+    y = 300
+    speed = 100
+end
+
+function love.update(dt)
+    local isDown = love.keyboard.isDown
+    if isDown("right") then
+        x = x + (speed * dt)
+    elseif isDown("left") then
+        x = x - (speed * dt)
+    end
+
+    if isDown("down") then
+        y = y + (speed * dt)
+    elseif isDown("up") then
+        y = y - (speed * dt)
+    end
+end
+
 function love.draw()
-    love.graphics.print('Hello world!', 400, 300)
+    love.graphics.print(text, x, y)
 end
