f9ca3fd74e51ecf6d18be083b45c318dcf59d80e
Hannu Leinonen <hleinone@gmail.com>
2010-01-04 05:26:28 +0200

    Enhanced walls, now they work somwhow.

--- main.lua
+++ main.lua
@@ -1,38 +1,38 @@
 function love.load()
     text = "hello world"
     speed = 500
-    world = love.physics.newWorld(0, 0, 1600, 1200, 0, 0)
+    world = love.physics.newWorld(800, 600)
     
     ballBody = love.physics.newBody(world, 50, 50, 0, 0)
-    ball = love.physics.newRectangleShape(ballBody, 0, 0, 70, 15)
+    ball = love.physics.newRectangleShape(ballBody, 0, 0, 10, 25)
     ballBody:setMassFromShapes()
 
     leftWallBody = love.physics.newBody(world, 0, 300)
-    leftWall = love.physics.newRectangleShape(leftWallBody, 0, 0, 10, 600)
+    leftWall = love.physics.newRectangleShape(leftWallBody, 0, 0, 5, 600)
     
-    rightWallBody = love.physics.newBody(world, 790, 300)
-    rightWall = love.physics.newRectangleShape(rightWallBody, 0, 0, 10, 600)
+    rightWallBody = love.physics.newBody(world, 730, 300)
+    rightWall = love.physics.newRectangleShape(rightWallBody, 0, 0, 5, 600)
     
-    roofBody = love.physics.newBody(world, 400, 590)
-    roof = love.physics.newRectangleShape(roofBody, 0, 0, 800, 10)
+    roofBody = love.physics.newBody(world, 400, 10)
+    roof = love.physics.newRectangleShape(roofBody, 0, 0, 800, 5)
     
-    floorBody = love.physics.newBody(world, 400, 0)
-    floor = love.physics.newRectangleShape(floorBody, 0, 0, 800, 10)
+    floorBody = love.physics.newBody(world, 400, 595)
+    floor = love.physics.newRectangleShape(floorBody, 0, 0, 800, 5)
 end
 
 function love.update(dt)
     local isDown = love.keyboard.isDown
     local x, y = ballBody:getPosition()
     if isDown("right") then
-        ballBody:applyForce(20, 0, 20, 20)
+        ballBody:applyForce(5, 0, 0, 0)
     elseif isDown("left") then
-        ballBody:applyForce(-20, 0, 20, 20)
+        ballBody:applyForce(-5, 0, 0, 0)
     end
 
     if isDown("down") then
-        ballBody:applyForce(0, 20, 20, 20)
+        ballBody:applyForce(0, 5, 0, 0)
     elseif isDown("up") then
-        ballBody:applyForce(0, -20, 20, 20)
+        ballBody:applyForce(0, -5, 0, 0)
     end
     world:update(dt)
 end
