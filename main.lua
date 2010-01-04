d9220dc124addd901a79feeab90fed5e88337a5f
Vesa Marttila <vesa.marttila@helsinki.fi>
2010-01-04 05:27:30 +0200

    Strange ball bouncing around.

--- main.lua
+++ main.lua
@@ -1,44 +1,45 @@
 function love.load()
-    text = "hello world"
-    speed = 500
-    world = love.physics.newWorld(0, 0, 1600, 1200, 0, 0)
-    ballBody = love.physics.newBody(world, 50, 50, 0, 0)
-    ball = love.physics.newRectangleShape(ballBody, 0, 0, 70, 15)
-    ballBody:setMassFromShapes()
+    world = love.physics.newWorld(-800, -600, 1600, 1200, 0, 0)
+    
+    x, y = 50, 50
+    ballBody = love.physics.newBody(world, x, y, 10, 0)
+    ball = love.physics.newCircleShape(ballBody, x, y, 1)
 
-    leftWallBody = love.physics.newBody(world, 0, 0)
-    leftWall = love.physics.newRectangleShape(leftWallBody, 0, 0, 10, 2000)
+    world:setCallbacks(function(t) print("COLLISION") end)
+--[[
+    leftWallBody = love.physics.newBody(world, 0, 300)
+    leftWall = love.physics.newRectangleShape(leftWallBody, 0, 0, 10, 600)
+    ]]
     
-    rightWallBody = love.physics.newBody(world, 790, 0)
-    rightWall = love.physics.newRectangleShape(rightWallBody, 0, 0, 10, 2000)
-    
-    roofBody = love.physics.newBody(world, 0, 590)
-    roof = love.physics.newRectangleShape(roofBody, 0, 0, 2000, 10)
-    
-    floorBody = love.physics.newBody(world, 0, 0)
-    floor = love.physics.newRectangleShape(floorBody, 0, 0, 2000, 10)
+    rightWallBody = love.physics.newBody(world, 790, 300)
+    rightWall = love.physics.newRectangleShape(rightWallBody, 0, 600, 3, 60)
+
+    roofBody = love.physics.newBody(world, 400, 60)
+    roof = love.physics.newRectangleShape(roofBody, 0, 0, 800, 3)
+
+    floorBody = love.physics.newBody(world, 400, 800)
+    floor = love.physics.newRectangleShape(floorBody, 0, 0, 800, 3)
 end
 
 function love.update(dt)
     local isDown = love.keyboard.isDown
-    local x, y = ballBody:getPosition()
+    x, y = ballBody:getPosition()
     if isDown("right") then
-        ballBody:applyForce(20, 0, 20, 20)
+        ballBody:applyForce(50, 0, 20, 20)
     elseif isDown("left") then
-        ballBody:applyForce(-20, 0, 20, 20)
+        ballBody:applyForce(-50, 0, 20, 20)
     end
 
     if isDown("down") then
-        ballBody:applyForce(0, 20, 20, 20)
+        ballBody:applyForce(0, 50, 20, 20)
     elseif isDown("up") then
-        ballBody:applyForce(0, -20, 20, 20)
+        ballBody:applyForce(0, -50, 20, 20)
     end
     world:update(dt)
 end
 
 function love.draw()
-    local x, y = ballBody:getPosition()
-    love.graphics.print(text, x, y)
+    love.graphics.circle("fill", x, y, 1, 16)
 end
 
 function love.keypressed(k)
